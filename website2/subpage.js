d037d2d75dfb6b5216f8bb4c7c89f30c2da7fa05
fmj23 <60016324+fmj23@users.noreply.github.com>
2020-11-15 22:45:03 -0700

    Delete subpage.js

--- website2/subpage.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded",bindButtons);
-
-function bindButtons(){
-	document.getElementById("homeButton").addEventListener('click',function(event){
-		console.log("Clicked for home");
-		window.location.href = "index.html";
-	});	
-}
